5a4101f500caed921abc68bc0aa121105e1e7e81
whovivekshukla <vivekshuklahere@gmail.com>
2023-01-23 19:09:22 +0530

    added few fixes

--- app.js
+++ app.js
@@ -40,23 +40,15 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use(express.static("./public"));
 app.use(fileUpload());
 
-app.get("/", (req, res) => {
-  res.send("ECommerce-API");
-});
-
-app.get("/api/v1", (req, res) => {
-  res.send("ECommerce-API");
-});
 
 // routes
-
+ 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
